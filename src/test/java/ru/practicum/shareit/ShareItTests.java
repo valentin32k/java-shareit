dbb0dfeb5f6a0207f369a016f16faaae2d2ef504
valentin32k <valentin32k@mail.ru>
2023-05-28 01:20:12 +0300

    Merge add-item-requests1

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -5,9 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ShareItTests {
-
-    @Test
-    void contextLoads() {
-    }
+//
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
